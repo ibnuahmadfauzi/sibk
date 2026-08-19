e3cfc254b3b22956057059e18ed32cdca0918af6
Aflahul Mukmin <aflahul.tasmanjr@gmail.com>
2026-08-19 11:21:59 +0700

    fix(reports): ensure badge_bg and icon_color keys are present with null-safe fallbacks

--- resources/views/pages/reports/index.blade.php
+++ resources/views/pages/reports/index.blade.php
@@ -18,7 +18,7 @@
                 <div class="col-12 col-md-6">
                     <div class="sibk-report-card h-100">
                         <div class="sibk-report-card__body">
-                            <div class="sibk-report-card__icon-wrapper" style="background-color: {{ $report['badge_bg'] }}; color: {{ $report['icon_color'] }};">
+                            <div class="sibk-report-card__icon-wrapper {{ $report['bg_class'] ?? '' }}" style="background-color: {{ $report['badge_bg'] ?? '#e9f2fb' }}; color: {{ $report['icon_color'] ?? '#2f6fc6' }};">
                                 <svg viewBox="0 0 24 24" width="22" height="22" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                     {!! $report['icon'] !!}
                                 </svg>
